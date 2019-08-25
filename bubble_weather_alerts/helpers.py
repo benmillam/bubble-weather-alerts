e9eb8ba736785428f85af83391b381b71effd286
benmillam <46112016+benmillam@users.noreply.github.com>
2019-08-25 11:58:13 -0700

    First tests and partial Location class

---
+++ bubble_weather_alerts/helpers.py
@@ -0,0 +1,5 @@
+class Location():
+	"""A Location"""
+	def __init__(self, zip = 95616):
+		self.zip = zip
+	
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from context import bubble_weather_alerts
 
 import unittest
 
-
-class BasicTestSuite(unittest.TestCase):
+class TestLocationClass(unittest.TestCase):
     """Basic test cases."""
-
-    def test_absolute_truth_and_meaning(self):
-        assert True
-
+    
+    def test_instantiate_location(self):
+        loc = bubble_weather_alerts.helpers.Location()
+        self.assertEqual(loc.__class__.__name__, 'Location')
+        
+    def test_set_default_zip(self):
+        loc = bubble_weather_alerts.helpers.Location() #no zip!
+        self.assertEqual(loc.zip,95616)
+        
+    def test_setting_zip(self):
+        loc = bubble_weather_alerts.helpers.Location(95618) #different zip!
+        self.assertEqual(loc.zip,95618)
 
 if __name__ == '__main__':
     unittest.main()
