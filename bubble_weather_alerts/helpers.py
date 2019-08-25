7664e55f4e59d1668cb07aee222afef5d2f0788e
benmillam <46112016+benmillam@users.noreply.github.com>
2019-08-25 12:44:41 -0700

    Location resolve zip to long lat
    via geopy and Nominatim

--- bubble_weather_alerts/helpers.py
+++ bubble_weather_alerts/helpers.py
@@ -2,4 +2,14 @@ class Location():
 	"""A Location"""
 	def __init__(self, zip = 95616):
 		self.zip = zip
+		self.lat = None
+		self.long = None
 	
+	def getLongLat(self):	
+		if ((self.lat is None) and (self.long is None)):
+			from geopy.geocoders import Nominatim
+			geolocator = Nominatim(user_agent="bubble_weather_alerts")
+			location = geolocator.geocode(str(self.zip))
+			self.lat = location.latitude
+			self.long = location.longitude
+		return (self.lat, self.long)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -18,6 +18,19 @@ class TestLocationClass(unittest.TestCase):
     def test_setting_zip(self):
         loc = bubble_weather_alerts.helpers.Location(95618) #different zip!
         self.assertEqual(loc.zip,95618)
+        
+    def test_get_long_lat_from_zip(self):
+        loc = bubble_weather_alerts.helpers.Location()
+        self.assertEqual(loc.getLongLat(),(38.5474853158191, -121.738295534409)) #95616 long lat via https://nominatim.openstreetmap.org/ (actually the website truncates some digits so you ended up pasting the full method result into the test later
+        
+    def test_need_call_getLongLat_to_set_attributes(self):
+        loc = bubble_weather_alerts.helpers.Location()
+        self.assertEqual((loc.lat,loc.long),(None,None)) #getLongLat hasn't set these!
+        
+    def test_getLongLat_sets_long_lat_attributes(self):
+        loc = bubble_weather_alerts.helpers.Location()
+        loc.getLongLat()
+        self.assertEqual((loc.lat,loc.long),(38.5474853158191, -121.738295534409)) #hrmm test isn't independent?
 
 if __name__ == '__main__':
     unittest.main()
