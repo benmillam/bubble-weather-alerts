d7cd393f8a6146454425f81bdafdff8fe9dad015
benmillam <46112016+benmillam@users.noreply.github.com>
2019-08-26 19:13:28 -0700

    refactor API into class, in progress

--- bubble_weather_alerts/helpers.py
+++ bubble_weather_alerts/helpers.py
@@ -4,53 +4,97 @@ class Location():
         self.zip = zip
         self.lat = None
         self.long = None
-    
+
     def truncate(self, n, decimals=6):
         multiplier = 10 ** decimals
         return int(n * multiplier) / multiplier
-    
-    def getLongLat(self):    
-        if ((self.lat is None) and (self.long is None)):
-            from geopy.geocoders import Nominatim
-            from math import floor
-            geolocator = Nominatim(user_agent="bubble_weather_alerts")
-            location = geolocator.geocode(str(self.zip))
-            self.lat = self.truncate(location.latitude) #truncate but don't round, Dark Sky was returning different final digita and API doesn't require that much precision
-            self.long = self.truncate(location.longitude)
+
+    def getLongLat(self):
+        maxtries = 3
+        tries = 0
+        while ((self.lat is None) and (self.long is None)):
+            try:
+                from geopy.geocoders import Nominatim
+                from math import floor
+                geolocator = Nominatim(user_agent="bubble_weather_alerts")
+                location = geolocator.geocode(str(self.zip))
+                self.lat = self.truncate(location.latitude) #truncate but don't round, Dark Sky was returning different final digita and API doesn't require that much precision
+                self.long = self.truncate(location.longitude)
+
+                tries = tries + 1
+                if (tries == maxtries):
+                    break
+            except:
+                tries = tries + 1
+                if (tries == maxtries):
+                    break
+
         return (self.lat, self.long)
-        
+
 class Weather():
-    
+
     def __init__(self, zip = 96516, historyDays = 5):
         self.history = {
             'temphigh': None,
             'templow': None,
             'humidity': None
         }
-        self.location = Location(zip).getLongLat()
-        self.lat = self.location[0]
-        self.long = self.location[1]
+        self.coordinates = Location(zip).getLongLat()
+        self.location = {
+                'lat': self.coordinates[0],
+                'long': self.coordinates[1]
+                }
         self.days_api_format = self.daysApi(historyDays)
-    
+        self.API = API()
+
     def daysApi(self,historyDays):
         from datetime import datetime, timedelta
         days = [(datetime.now() - timedelta(days=n)) for n in list(range(1,historyDays + 1))]
         #[YYYY]-[MM]-[DD]T[HH]:[MM]:[SS]
         return [d.strftime("%Y-%m-%d") for d in days]
-    
-    def getDayData(self, day):
-        import requests
-        import sys
+
+    def getForecast(self):
+        self.forecast = self.API.getData(self.location, forecast = True)
+    def getHistory(self):
+        pass
+
+
+class API():
+    """Dark Sky API"""
+    def __init__(self):
         # insert at 1, 0 is the script path (or '' in REPL)
+        import sys
         sys.path.insert(1, './APIs/')
         # REDALERT need check best practice for this, make methods etc 'private' (by convention)
         from apis import dark_sky_api_secret_key
         from apis import dark_sky_api_endpoint
-        api_url_end = 'T12:00:00?exclude=minutely,hourly'
-        url = dark_sky_api_endpoint + dark_sky_api_secret_key + '/' + str(self.lat) + ',' + str(self.long) + ',' + day + api_url_end
-        response = requests.get(url)
-        response.raise_for_status()
+
+        self.key = dark_sky_api_secret_key
+        self.endpoint = dark_sky_api_endpoint
+        self.api_time = 'T00:00:00'
+        self.api_exclude = '?exclude=minutely'
+
+    def getData(self, location, day = None, forecast = False):
+        import requests
+        if (forecast):
+            url = self.endpoint + self.key + '/' + str(location['lat']) + ',' + str(location['long']) + ',' + self.api_exclude
+        else:
+            from datetime import datetime
+            if (day is None):
+                day = datetime.now().strftime("%Y-%m-%d")      #[YYYY]-[MM]-[DD]T[HH]:[MM]:[SS]
+            url = self.endpoint + self.key + '/' + str(location['lat']) + ',' + str(location['long']) + ',' + day + self.api_time + self.api_exclude
+
+        maxtries = 3
+        tries = 0
+        while True:
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                print('Hey what is happening here endless loop...')
+                break
+            except:
+                tries += 1
+                if (tries == maxtries):
+                    return None
+                continue
         return response.json()
-    
-    def getHistory(self):
-        pass
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,59 +6,96 @@ import unittest
 
 class TestLocationClass(unittest.TestCase):
     """Basic test cases."""
-    
+
     def test_instantiate_location(self):
         loc = bubble_weather_alerts.helpers.Location()
         self.assertEqual(loc.__class__.__name__, 'Location')
-        
+
     def test_set_default_zip(self):
         loc = bubble_weather_alerts.helpers.Location() #no zip!
         self.assertEqual(loc.zip,95616)
-        
+
     def test_setting_zip(self):
         loc = bubble_weather_alerts.helpers.Location(95618) #different zip!
         self.assertEqual(loc.zip,95618)
-        
+
     def test_get_long_lat_from_zip(self):
         loc = bubble_weather_alerts.helpers.Location()
         self.assertEqual(loc.getLongLat(),(38.547485, -121.738295)) #95616 long lat via https://nominatim.openstreetmap.org/ (actually the website truncates some digits so you ended up pasting the full method result into the test later
-        
+
     def test_need_call_getLongLat_to_set_attributes(self):
         loc = bubble_weather_alerts.helpers.Location()
         self.assertEqual((loc.lat,loc.long),(None,None)) #getLongLat hasn't set these!
-        
+
     def test_getLongLat_sets_long_lat_attributes(self):
         loc = bubble_weather_alerts.helpers.Location()
         loc.getLongLat()
         self.assertEqual((loc.lat,loc.long),(38.547485, -121.738295)) #hrmm test isn't independent?
-        
+
 class TestWeatherClass(unittest.TestCase):
     """Basic test cases."""
-    
+
     def test_instantiate_weather(self):
         weather = bubble_weather_alerts.helpers.Weather()
         self.assertEqual(weather.__class__.__name__, 'Weather')
-        
+
     def test_need_call_their_methods_to_set_attributes(self):
         weather = bubble_weather_alerts.helpers.Weather()
         self.assertEqual((weather.history['temphigh'],weather.history['templow'],weather.history['humidity']),(None,None, None)) #e.g. getHistory hasn't set these!
-    
+
     def test_set_list_of_days_formatted_for_Dark_Sky_API(self):
         weather = bubble_weather_alerts.helpers.Weather(historyDays = 7)
         self.assertEqual(len(weather.days_api_format),7)
         self.assertEqual(len(weather.days_api_format[0]),10) #e.g. 2019-07-06, weak but moving on
-    
-    def test_getDayData_returns_API_data(self):
+
+    def test_instantiate_API_on_Weather(self):
+        weather = bubble_weather_alerts.helpers.Weather()
+        self.assertEqual(weather.API.__class__.__name__, 'API')
+
+    def test_getForecast_returns_API_data(self):
         weather = bubble_weather_alerts.helpers.Weather(95618)
-        
-        data = weather.getDayData("2019-08-24")
-        assert(data)
-    
+        weather.getForecast()
+        assert(weather.forecast)
+
+    def test_where_left_off(self):
+        #getDayData returns a historical day's weather data, need look through and determine exactly what we'll store like per the next test
+        assert(False)
+
     def test_getHistory_sets_lists(self):
         weather = bubble_weather_alerts.helpers.Weather()
         weather.getHistory()
         self.assertEqual((type(weather.history['temphigh']),type(weather.history['templow']),type(weather.history['humidity'])), (list,list,list))
-        
+
+
+class TestAPIClass(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_instantiate_api(self):
+        api = bubble_weather_alerts.helpers.API()
+        self.assertEqual(api.__class__.__name__, 'API')
+
+    def test_api_imports_configs(self):
+        api = bubble_weather_alerts.helpers.API()
+        assert(api.key)
+        assert(api.endpoint)
+
+    def test_api_getData_method(self):
+        api = bubble_weather_alerts.helpers.API()
+        assert(api.getData)
+
+    def test_getData_returns_API_data(self):
+        api = bubble_weather_alerts.helpers.API()
+        location = {
+                'lat': 38.547485,
+                'long': -121.738295
+                }
+        data = api.getData(location, forecast = False)
+        assert(data)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
